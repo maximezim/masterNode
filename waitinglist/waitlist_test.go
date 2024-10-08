b12e9f7d764e8c60a0eca5ae6e2d94cbfd58b9b7
Maxime Zimmermann <92118856+maximezim@users.noreply.github.com>
2024-10-08 09:57:51 +0200

    Waitinglist (#1)
    * added waitinglist
    
    * made adjusments
    
    ---------
    
    Co-authored-by: 0xf7ed0 <sausse.sylvain88@gmail.com>

---
+++ waitinglist/waitlist_test.go
@@ -0,0 +1,25 @@
+package waitinglist
+
+import (
+	"bytes"
+	"crypto/rand"
+	"testing"
+)
+
+func TestFeedAndTake(t *testing.T) {
+	const iterations int = 100
+	var feeder [100][]byte
+	list := NewWaitingList(iterations)
+	for i := range iterations {
+		feeder[i] = make([]byte, 1920*1080*3)
+		rand.Read(feeder[i])
+		list.AddContent(feeder[i])
+	}
+	for i := range iterations {
+		b := list.GetContent()
+		if !bytes.Equal(feeder[i], *b) {
+			t.Error()
+			return
+		}
+	}
+}
